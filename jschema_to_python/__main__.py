f6e36334cadf4eb45712ee71cef9d010a6e2d520
Larry Golding <larrygolding@comcast.net>
2019-09-17 16:42:48 -0700

    Add a __main__ to allow directly executing the package.

---
+++ jschema_to_python/__main__.py
@@ -0,0 +1,13 @@
+from __future__ import absolute_import
+
+import os
+import sys
+
+if __package__ == '':
+    path = os.path.dirname(os.path.dirname(__file__))
+    sys.path.insert(0, path)
+
+from jschema_to_python.driver import main as _main  # noqa
+
+if __name__ == '__main__':
+    sys.exit(_main())
