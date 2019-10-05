c27fb6ae1bb09de5b39005f234911e55662fd6d9
Larry Golding <larrygolding@comcast.net>
2019-10-05 12:08:32 -0700

    DRY out class generator tests.

--- tests/test_class_generator.py
+++ tests/test_class_generator.py
@@ -4,38 +4,37 @@ import jschema_to_python.class_generator as cg
 
 
 def test_required_property(tmp_path):
-    class_generator = cg.ClassGenerator(
+    verify(
         class_schema={"properties": {"req": {"type": "int"}}, "required": ["req"]},
         class_name="RequiredProperty",
-        code_gen_hints=None,
-        output_directory=str(tmp_path),
+        expected_file_name="_required_property.py",
+        tmp_path=tmp_path,
     )
-    class_generator.generate()
-
-    with open(class_generator.file_path, "r") as fileObj:
-        actual = fileObj.read()
-
-    with open("tests/test_files/_required_property.py", "r") as fileObj:
-        expected = fileObj.read()
-
-    expected = expected.replace("$js2p_version$", js2p.__version__)
-
-    assert actual == expected
 
 
 def test_optional_property(tmp_path):
-    class_generator = cg.ClassGenerator(
+    verify(
         class_schema={"properties": {"opt": {"type": "int", "default": 42}}},
         class_name="OptionalProperty",
+        expected_file_name="_optional_property.py",
+        tmp_path=tmp_path,
+    )
+
+
+def verify(class_schema, class_name, expected_file_name, tmp_path):
+    class_generator = cg.ClassGenerator(
+        class_schema=class_schema,
+        class_name=class_name,
         code_gen_hints=None,
         output_directory=str(tmp_path),
     )
+
     class_generator.generate()
 
     with open(class_generator.file_path, "r") as fileObj:
         actual = fileObj.read()
 
-    with open("tests/test_files/_optional_property.py", "r") as fileObj:
+    with open("tests/test_files/" + expected_file_name, "r") as fileObj:
         expected = fileObj.read()
 
     expected = expected.replace("$js2p_version$", js2p.__version__)
